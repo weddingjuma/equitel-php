f64ce5e71ab1e869bb454187a666ad51faaf593c
Mauko Maunde <maukoese@gmail.com>
2017-12-23 18:45:33 +0300

    Create Response.php

---
+++ Response.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @package Equitel PHP Library
+ * @subpackage Response Utility Class
+ * @version 0.18.01
+ * @author Mauko Maunde < hi@mauko.co.ke >
+ * @link https://developers.equitybankgroup.com/
+  */
