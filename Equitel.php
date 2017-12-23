4efe896dd732a53715d74bd733490fd3b16b79e2
Mauko Maunde <maukoese@gmail.com>
2017-12-23 18:47:20 +0300

    Update Equitel.php

--- Equitel.php
+++ Equitel.php
@@ -1,1 +1,16 @@
 
+<?php
+/**
+ * @package Equitel PHP Library
+ * @subpackage Equitel Core Class
+ * @version 0.18.01
+ * @author Mauko Maunde < hi@mauko.co.ke >
+ * @link https://developers.equitybankgroup.com/
+ */
+
+class Equitel
+{
+  public function __construct()
+  {
+  }
+}
